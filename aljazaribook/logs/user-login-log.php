f13ee42b6541041d6432a6668a98988afb901a02
behlulaksu <behlul.aksu@hotmail.com>
2024-09-18 11:07:08 +0300

    log file

---
+++ aljazaribook/logs/user-login-log.php
@@ -0,0 +1,118 @@
+<?php /* Template Name: User Login Log */ ?>
+
+<?php 
+if (isset($_GET['userID'])){
+	$userID = strip_tags($_GET["userID"]); 
+}else{
+	?>
+	<script>
+		window.location.href = "<?php echo get_site_url(); ?>";
+	</script>
+	<?php 
+
+}
+$blog_id = get_current_blog_id();
+$user = get_user_by('id', $userID);
+
+global $wpdb;
+$book_objective = "user_login_logs";
+$query = $wpdb->prepare("SELECT * from $book_objective where user_id =".$userID );
+$sonuclar = $wpdb->get_results($query);
+?>
+
+<?php get_header(); ?>
+<!-- DataTables -->
+<link href="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+<!-- Responsive datatable examples -->
+<link href="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" /> 
+
+<div class="main-content">
+	<div class="page-content dark:bg-zinc-700">
+		<div class="container-fluid px-[0.625rem]">
+			<h4 class="display_name">
+				<?php echo $user->display_name; ?> 
+				<a href="mailto:<?php echo $user->user_email; ?>">
+					( <?php echo $user->user_email; ?> )
+				</a>
+			</h4>
+			<div class="col-span-12" style="margin-top: 20px;">
+				<div class="card dark:bg-zinc-800 dark:border-zinc-600">
+					<div class="card-body relative overflow-x-auto">
+						<table id="datatable-buttons" class="table w-full pt-4 text-gray-700 dark:text-zinc-100">
+							<thead>
+								<tr>
+									<th class="p-4 pr-8 border rtl:border-l-0 border-y-2 border-gray-50 dark:border-zinc-600">Log ID</th>
+									<th class="p-4 pr-8 border rtl:border-l-0 border-y-2 border-gray-50 dark:border-zinc-600">IP</th>
+									<th class="p-4 pr-8 border border-y-2 border-gray-50 dark:border-zinc-600 border-l-0">Date/Time</th>
+									<th class="p-4 pr-8 border border-y-2 border-gray-50 dark:border-zinc-600 border-l-0">Campus - Year</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php  
+								foreach ($sonuclar as $key => $value) {
+									?>
+									<tr>
+										<td class="p-4 pr-8 border rtl:border-l-0 border-t-0 border-gray-50 dark:border-zinc-600">
+											<?php echo $value->id; ?>
+										</td>
+										<td class="p-4 pr-8 border rtl:border-l-0 border-t-0 border-gray-50 dark:border-zinc-600">
+											<?php echo $value->ip_adress; ?>
+										</td>
+										<td class="p-4 pr-8 border border-t-0 border-l-0 border-gray-50 dark:border-zinc-600">
+											<?php echo $value->login_time; ?>
+										</td>
+										<td class="p-4 pr-8 border border-t-0 border-l-0 border-gray-50 dark:border-zinc-600">
+											<?php
+											$site_details = get_blog_details($value->site_id);
+											echo $site_details->blogname; 
+											?>
+										</td>
+									</tr>
+									<?php 
+								}
+								?>
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.display_name{
+		background-color: #8e1838;
+		color: #fff;
+		padding: 10px;
+		border-radius: 10px;
+	}
+</style>
+<script>
+	var get_site_url = <?php echo "'".get_site_url()."'"; ?>;
+	var get_template_url = <?php echo "'".get_bloginfo('template_directory')."'"; ?>;
+	var userID = <?php echo $userID; ?>
+</script>
+<?php get_footer(); ?>
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/jszip/jszip.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/pdfmake/build/pdfmake.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/pdfmake/build/vfs_fonts.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+
+<!-- Responsive examples -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+<!-- Datatable init js -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/pages/datatables.init.js"></script> 
